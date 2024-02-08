288254973da38ebb8564bd47f32605e6623d802b
Tim Reynolds <timothy.reynolds@utexas.edu>
2024-02-08 13:19:08 -0600

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _1001Albums.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
